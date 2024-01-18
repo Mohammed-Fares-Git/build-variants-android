c2a3ea8d4f6f0cbe96d2ba007f0deb340aee4d86
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-18 16:50:42 +0100

    use the buildConfig to render dferent content

--- app/src/main/java/com/mohammedfares/buildvariants/MainActivity.kt
+++ app/src/main/java/com/mohammedfares/buildvariants/MainActivity.kt
@@ -2,14 +2,35 @@ package com.mohammedfares.buildvariants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.BuildCompat
+import com.mohammedfares.buildvariants.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        if (BuildConfig.FREE && BuildConfig.DEBUG){
+            binding.root.setBackgroundColor(getColor(R.color.black))
+            binding.mainTextView.text = "this is free and debug"
+        }
 
+        if (BuildConfig.FREE && !BuildConfig.DEBUG){
+            binding.root.setBackgroundColor(getColor(R.color.blue))
+            binding.mainTextView.text = "this is free and release"
+        }
 
+        if (!BuildConfig.FREE && BuildConfig.DEBUG){
+            binding.root.setBackgroundColor(getColor(R.color.green))
+            binding.mainTextView.text = "this is paid and debug"
+        }
+
+        if (!BuildConfig.FREE && !BuildConfig.DEBUG){
+            binding.root.setBackgroundColor(getColor(R.color.red))
+            binding.mainTextView.text = "this is paid and release"
+        }
 
     }
 }
